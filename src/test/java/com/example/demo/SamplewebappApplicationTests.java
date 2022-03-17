6ffd89b951111d3a47363424aa85b0bec8149f05
csvishnu <csvishnu195@gmail.com>
2022-03-17 12:38:39 +0530

    changes

--- src/test/java/com/example/demo/SamplewebappApplicationTests.java
+++ src/test/java/com/example/demo/SamplewebappApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@componentscan(basePackages= {"com.tryingsboot.controller"})
 class SamplewebappApplicationTests {
 
 	@Test
